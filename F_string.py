8ce8de0cdaefce4de2fd2abc89589fa7747f56a6
CodeWithZavi <nomiemerge9901@gmail.com>
2026-01-22 12:50:06 +0500

    //

--- F_string.py
+++ F_string.py
@@ -1,3 +1,17 @@
 name =  "Sardar Zavian"
 age = 20
 print(f"my name is {name} and I am {age} years old.")
+
+
+
+#now if i eant directly to print the string without assigning it to a variable
+print(f"my name is {'Sardar Zavian'} and I am {20} years old.")
+ 
+print(f"my name is {{'Sardar Zavian'}} and I am {{20}} years old.")
+
+#docstring literals
+def my_function():
+    """This is a docstring. It is used to describe what the function does."""
+    return None 
+print(my_function.__doc__)  # prints the docstring of the function
+ 
